86800365d5fe743cd3d2cac3578cf2492174cbf1
Penny55gg <63138214+Penny55gg@users.noreply.github.com>
2025-08-21 10:18:44 -0400

    Update ???-shop-fix.js

--- ???-shop-fix.js
+++ ???-shop-fix.js
@@ -3,28 +3,30 @@
 
 (function() {
     ShopListScreen._getShopEntryArray = function() {
-		var i, data, entry;
-		var arr = [];
-		var list = this._getDataList();
-		var count = list.getCount();
-		
-		for (i = 0; i < count; i++) {
-			data = list.getData(i);
-			entry = StructureBuilder.buildListEntry();
-			
-			entry.isAvailable = data.isShopDisplayable();
-			if (entry.isAvailable) {
-				entry.name = data.getName();
-			}
-			else {
-                continue
-			}
-			entry.data = data;
-			
-			arr.push(entry);
-		}
-		
-		return arr;
-        }
-})();
+        var i, data, entry;
+        var arr = [];
+        var list = this._getDataList();
+        var count = list.getCount();
 
+        for (i = 0; i < count; i++) {
+            data = list.getData(i);
+            entry = StructureBuilder.buildListEntry();
+
+            entry.isAvailable = data.isShopDisplayable();
+            if (entry.isAvailable) {
+                entry.name = data.getName();
+            }
+            else if (i == count - 1) {
+                entry.name = StringTable.HideData_Question;
+            }
+            else {
+                continue;
+            }
+            entry.data = data;
+
+            arr.push(entry);
+        }
+
+        return arr;
+    }
+})();
